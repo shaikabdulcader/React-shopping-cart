76d7355654c79145f07adf59c3d08ed5dc620aa7
shaikabdulcader <shaikabdulcader12345@gmail.com>
2024-01-23 06:50:52 +0530

    initial

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,18 +1,8 @@
-function Footer() {
-  return (
-    <footer className="footer-section">
-      <p>
-        © {new Date().getFullYear()}{" "}
-        <a
-          href="https://www.nike.com/in/w/football-shoes-1gdj0zy7ok"
-          target="_blank"
-          className="link"
-        >
-          Nike
-        </a>{" "}
-        , Inc. All Rights Reserved
-      </p>
-    </footer>
-  );
+function Footer(){
+    return(
+        <footer className="footer-section">
+            <p>&copy; {new Date().getFullYear()} <a href="https://www.nike.com/in/w/football-shoes-1gdj0zy7ok" target="_blank" className="link">Nike</a> , Inc. All Rights Reserved</p>
+        </footer>
+    );
 }
 export default Footer;
